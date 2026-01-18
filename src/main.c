01124005ef60f83e3294b04725a2a414ab026d95
knot <arjablc@gmail.com>
2026-01-19 00:18:51 +0545

    repl

--- src/main.c
+++ src/main.c
@@ -5,12 +5,15 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
+  while (1==1) {
   printf("$ ");
-  char input[1024] = "";
+  char input[1024];
   fgets(input, sizeof(char) * 1024, stdin);
   if(input != NULL){
     input[strcspn(input, "\n")] = '\0';
     printf("%s: command not found", input);
+    printf("\n");
+  }
   }
   return 0;
 }
