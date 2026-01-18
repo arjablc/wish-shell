d03d14506c6e3b22c6795882024b8d95e7c01794
knot <arjablc@gmail.com>
2026-01-19 00:52:18 +0545

    fixed after refactor'

--- src/main.c
+++ src/main.c
@@ -3,45 +3,32 @@
 
 #define INPUT_SIZE 1024
 
-static void handle_echo(const char *input) {
-    const char *msg = strchr(input, ' ');
-    if (msg != NULL) {
-        printf("%s\n", msg + 1);
-    } else {
-        printf("\n");
-    }
-}
+static void handle_echo(const char *input) {}
 
 int main(void) {
-    setbuf(stdout, NULL);
+  setbuf(stdout, NULL);
+  char input[INPUT_SIZE];
+  while (1) {
+    printf("$ ");
 
-    char input[INPUT_SIZE];
-
-    while (1) {
-        printf("$ ");
-
-        if (fgets(input, INPUT_SIZE, stdin) == NULL) {
-            break; /* EOF or error */
-        }
-
-        /* remove trailing newline */
-        input[strcspn(input, "\n")] = '\0';
-
-        if (strcmp(input, "exit") == 0) {
-            break;
-        }
-
-        char *command = strtok(input, " ");
-        if (command == NULL) {
-            continue;
-        }
-
-        if (strcmp(command, "echo") == 0) {
-            handle_echo(input);
-        } else {
-            printf("%s: command not found\n", command);
-        }
+    if (fgets(input, INPUT_SIZE, stdin) == NULL) {
+      break;
+    }
+    input[strcspn(input, "\n")] = '\0';
+    if (strcmp(input, "exit") == 0) {
+      break;
+    }
+    char *command = strtok(input, " ");
+    if (command == NULL) {
+      continue;
     }
 
-    return 0;
+    if (strcmp(command, "echo") == 0) {
+      printf("%s\n", input + strlen(command) + 1);
+    } else {
+      printf("%s: command not found\n", command);
+    }
+  }
+
+  return 0;
 }
