f717e5ccd4aac8d4bd919da1b0f973ec0c431c5e
knot <arjablc@gmail.com>
2026-01-18 23:49:51 +0545

    fix: fixed the output

--- src/main.c
+++ src/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
+  printf("$ ");
   char input[1024] = "";
   fgets(input, sizeof(char) * 1024, stdin);
-  printf("$ %s", input);
+  if(input != NULL){
+    input[strcspn(input, "\n")] = '\0';
     printf("%s: command not found", input);
-
+  }
   return 0;
 }
